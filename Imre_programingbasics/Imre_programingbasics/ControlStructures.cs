bbbab40f134d1407e63027475a8d9d77674b0170
Crymric <crymric@gmail.com>
2023-01-14 16:59:50 +0100

    First Lecture

---
+++ Imre_programingbasics/Imre_programingbasics/ControlStructures.cs
@@ -0,0 +1,164 @@
+﻿/* Console.WriteLine("Hello Wrold!");
+
+// Elágazások
+
+int ammo = 10;
+bool isInAir = false;
+
+if (ammo > 0 && !isInAir)
+{
+    Console.WriteLine("BUMM");
+    ammo--; 
+}
+
+bool isGrounded = !isInAir;
+bool haveAmmo = ammo > 0;
+bool canShoot = haveAmmo && isGrounded;
+if (canShoot)
+{
+    Console.WriteLine("BUMM");
+}
+else
+{
+    Console.WriteLine("KLICK");
+}
+
+//------------
+
+Console.WriteLine("Írj be egy egész számot!");
+string line = Console.ReadLine();
+int number = int.Parse(line);
+
+Console.WriteLine("A szám duplája:");
+Console.WriteLine(number * 2);
+*/
+
+
+
+/*
+Console.WriteLine("Írj be egy egész számot!");
+// Páros vagy páratlan
+string line = Console.ReadLine();
+int number = int.Parse(line);
+
+bool isEven = (number % 2) == 0;
+if (isEven)
+{
+    Console.WriteLine("Páros");
+}
+else
+{
+    Console.WriteLine("Páratlan");
+}
+*/
+
+/*
+//--------------------------
+//Pozitív, negatív
+
+Console.WriteLine("Írj be egész számot!");
+string line = Console.ReadLine();
+int number = int.Parse(line);
+
+if (number > 0)
+{
+    Console.WriteLine("Pozitív");
+}
+else if (number < 0) //különben ha
+{
+    Console.WriteLine("Negatív");
+}
+else
+{
+    Console.WriteLine("Nulla");
+}
+
+
+
+//-------------
+// Kerek, nem kerek
+
+Console.WriteLine("Írj be egész számot!");
+string line = Console.ReadLine();
+int number = int.Parse(line);
+
+string text = number % 10 == 0 ? "Kerek" : "Nem kerek"; //feltételes értékadás operátor, ? elsődleges ha ez nem jó akkor a : utáni értéket adja
+Console.WriteLine();
+
+*/
+
+//---------------- CIKLUSOK -----------------------
+/*
+int i = 1;
+
+while (i <= 100) 
+{
+    Console.WriteLine(i);
+    i++;
+}
+
+for (int j = 1; j <= 100; j++)
+{
+    Console.WriteLine(j);
+}
+*/
+/*
+//--------------------
+// Első 10 szám, ami osztható 3-al
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i * 3);
+}
+*/
+
+//----------------------
+//Első n olyan természetes szám, ami osztható 5-el, de nem osztható 7-el
+/*
+Console.WriteLine("Hány számot írjak ki?");
+string line = Console.ReadLine();
+int n = int.Parse(line);
+
+int x = 5;
+int count = 0;
+
+for (int i = 1; count < n; i++)
+{
+    int numberToWrite = i * x;
+
+    bool devisibleWith7 = numberToWrite % 7 == 0;
+
+    if (!devisibleWith7)
+    {
+     Console.WriteLine(numberToWrite);
+        count++;
+    }
+}
+
+*/
+
+//----------------
+//Szorzótábla
+/*
+for (int i = 1; i <= 10; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        int result = i * j;
+        Console.WriteLine(j + " * " + i + " = " + result);
+    }
+}
+*/
+
+//-------------------
+// Metódusok
+
+double f1 = 12.5f;
+double f2 = 3;
+
+double power = Math.Pow(f1,f2);
+double power2 = Math.Pow(45, 11.5);
+
+double f3 = Math.Abs(f1);
+
+double pi = Math.PI;
---
+++ Imre_programingbasics/Imre_programingbasics/Program.cs
@@ -0,0 +1,121 @@
+﻿// Console.WriteLine("Hello World!");
+// comment, nem számít bele a kódba, jegyzetelésre, az egész sor komment lesz
+/* szintén komment, beleilleszthető a kódsorba */
+/*
+int age = 35; // int egész számos változó, ez egy típus, csharp típusos nyelv, int 32 biten tárol 
+Console.WriteLine(age); 
+age = 36;
+Console.WriteLine(age);
+
+int height; //deklaráció
+height = 182; //definíció
+Console.WriteLine(height);
+
+int abc = age + 5 + height - 3; 
+Console.WriteLine(abc);
+Console.WriteLine(age + 5);
+
+int a, b, c;
+
+a = 5;
+b = 7;
+c = a + b;
+c = 25; //c értékét 25re módosítja és nem a+b összege lesz
+Console.WriteLine(c);
+
+// Műveletek
+int sum = a + b; // 12   operátor az összeadás, operandus az a
+int dif = a - b; // -2
+int product = a * b; // 35 
+int rate = a / b; // int csak egész lehet, ezért ez 0 lesz
+
+Console.WriteLine(rate);
+//modulo a maradékot írja le
+int modulo1 = 10 % 3; // 1 
+int modulo2 = 12 % 4; // 0
+int modulo3 = 6 % 5; // 1
+int modulo4 = 100 % 8; // 4
+int modulo5 = 12 % 16; // 12
+
+int num1 = 3 + 2 * 4; // 11, C#-ban van műveleti sorrend
+int num2 = (3 + 2) * 4; //20
+
+int number = 0;
+
+number = number + 5; // 5, number értéke legyen egyenlő a számmal
+number += 5;         // 10
+
+number -= 2;         // 8
+number *= 2;         // 16
+number /= 3;         // 5
+number %= 3;         // 2    sorba számoljuk, sorrol sorra, előző sor értékéből indulunk
+
+number += 1;         // 3
+number++;            // 4
+
+number -= 1;         // 3
+number--;            // 2 
+
+//----------------------
+
+float f1 = 12.5f; // floating point number, tört számokat is tudja tárolni, 32 biten (4 byte), lebeg a tizedes pont
+double d1 = 12.77f; // ez is tud tört számokat tárolni, de más mennyiségű biten tárolja, 64 biten (8 byte)
+
+float f2 = 12.45f;
+float f3 = 3 + f2;
+
+// Casting - kasztolás - egy típust átalakítani egy másikba
+
+int i1 = 7;
+float f4 = 33.5f;
+
+int i2 = (int) f4; //int-be floatot nem rakhatunk, ha tört, mert az int azt nem kezeli, ez egy explicit kasztolás, el fog veszni a vessző utáni rész
+float f5 = i1; // implicit kasztolás, explicitbe jelölni kell, imlicitbe elhagyható a ()
+
+//--------------------------------
+
+string myOwnSpeciálString_1 = "valami szöveg"; //karakterlánc, lehet benne szám, de nem az elején
+
+string s1 = f1 + "AAA" + " " + i1 + "BBB"; // a + operátor jel itt összefűzést jelent, az AAA és a BBB operandust fűzi össze
+
+string s2 = 3 + i1 + "AAA"; // összefőzés és az összeadás ugyanazon a szinten van, ezért sorba végzi
+
+int xxx = int.Parse("10"); // azért "10" mert string van benne, ez egy stringé való alakítás
+xxx += 5;
+
+int hex = 0x1A45F3B;
+int bin = 0b10100101;
+
+Console.WriteLine(bin);
+
+//-------------------
+
+bool b1 = false; // két értéket tud csak felvenni 1 vagy 0, false or true, bool vagy logikai típus
+bool b2 = true;
+
+bool b3 = i1 > f3; // eldönti, hogy nagyobb e
+Console.WriteLine(b3);
+bool b4 = i1 < f3;
+bool b5 = i1 >= 43;
+bool b6 = 22.4f <= f3;
+
+bool b7 = 3 > 3;
+bool b8 = 3 >= 3;
+
+bool b9 = i1 == f3;    // egyenlőség vizsgálat operátor, ha igaz, hogy egyenlő akkor true, ha nem akkor false
+bool b10 = i1 != f3;    // egyenlőtlenség vizsgálat
+
+bool b11 = "aaabbb" == "aaaBBB"; // false
+bool b12 = "aaabbb" == "aaabbb"; // true
+
+//-----------------------------
+
+bool b13 = b1 == b2; // b13-nak az lesz az értéke, hogy megegyezik e a b1 és b2
+
+bool b14 = b1 && b2; // ÉS: Iga, ha mindkettő igaz
+bool b15 = b1 || b2; // VAGY: Igaz, ha legalább az egyik az
+
+bool b16 = !b1;      // NEGÁLÁS: b16-ba kerüljön be az ellentéte, a másik
+
+bool b17 = b1 ^ b2; // XOR: kizáró vagy, ha az A igaz vagy a B igaz, de nem mind a kettő 
+ */ 
